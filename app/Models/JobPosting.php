d16e6e64ad3c946b60ba2ea5bec3816e29f9e3b6
itsmrsarfaraz <itsmrsarfaraz@gmail.com>
2026-07-18 02:05:42 +0500

    fix(test): added HasFactory to JobPosting

--- app/Models/JobPosting.php
+++ app/Models/JobPosting.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use App\Enums\EmploymentType;
 use App\Enums\JobPostingStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class JobPosting extends Model
 {
+    use HasFactory;
+
     protected $guarded = ['id'];
 
     protected function casts(): array
